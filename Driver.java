3df4702d92ebba337960769bd0700242a12a59ed
RyanWhyte2000 <56200781+RyanWhyte2000@users.noreply.github.com>
2021-03-12 11:12:51 -0300

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -7,41 +7,77 @@ import models.Student;
 public class Driver {
 
 	public static void main(String[] args) {
+		Student stud  = new Student();
+		Scanner in = new Scanner(System.in);
+		int choice=0;
+		do {
+			System.out.println("1. Create ");
+			System.out.println("2. Readall ");
+			System.out.println("3. Update");
+			System.out.println("4. Delete");
+			System.out.println("5. Exit ");
+			
+			
+			choice= in.nextInt();
+			
+			switch(choice) {
+			
+			case 1:
+				System.out.println(" Enter ID");
 
-	Student stud  = new Student();
-	
-	Scanner input = new Scanner(System.in);
-	
-	
-	System.out.println(" Enter ID");
-	
-	String id= input.next();
-	
-	System.out.println(" Enter Name");
+				String id= in.next();
 
-	String name= input.next();
+				System.out.println(" Enter first  Name");
 
-	stud.create(id, name);
-	stud.readAll(); 
-	
-	System.out.println(" Enter ID to be updated");
-	
-	String updateId= input.next();
-	
-	System.out.println(" Enter Name to be updated");
+				String firstName= in.next();
+			
+				System.out.println(" Enter last  Name");
 
-	String updateName= input.next();
+				String lastName= in.next();
+
+				System.out.println(" Enter email");
+
+				String email= in.next();
+
+
+				stud.create(id, firstName, lastName, email);
+				
+				break;
+			case 2:
+				stud.readAll(); 
+
+				
+				break;
+			case 3:
+				System.out.println(" Enter ID to be updated");
+
+				String updateId= in.next();
+
+				System.out.println(" Enter Name to be updated");
+
+				String updateName= in.next();
+
+				stud.update(updateId, updateName);
+				break;
+			case 4:
+				System.out.println(" Enter ID you want to delete");
+
+				String deletedId= in.next();
+				stud.delete(deletedId);
+
+				stud.readAll();
+				break;
+		
+			case 5:
+					System.out.println(" Exiting........");
+				break;
+			}
+			
+		}while (choice !=5);
 	
-	stud.update(updateId, updateName);
-	stud.readAll(); 
-	System.out.println(" Enter ID you want to delete");
-	
-	String deletedId= input.next();
-	stud.delete(deletedId);
-	
-	stud.readAll();
-	
-	
+
+
+
 	}
 
 }
--- Student.java
+++ Student.java
@@ -11,32 +11,32 @@ import javax.swing.JOptionPane;
 
 public class Student {
 
-	
+
 	private  Connection connect;
-	
+
 	public Student() {
 		String url = "jdbc:mysql://localhost:3306/";
 
 		try {
 			connect= DriverManager.getConnection(url, "root", "");
 			if(connect != null) {
-				
+
 				JOptionPane.showMessageDialog(null, " Connected to local Server",
-             	" JDBC Conection Status", JOptionPane.INFORMATION_MESSAGE);
-					
-			}
-			
-		
+						" JDBC Conection Status", JOptionPane.INFORMATION_MESSAGE);
+
+			} 
+
+
 		}catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public void create(String id, String name) {
-		
-		
-		String sql= "INSERT INTO dblab.students(ID,Name) VALUES (" + id + ", '" +name+"');";
-		
+	public void create(String id, String firstName,String lastName,String email ) {
+
+
+		String sql= "INSERT INTO dblab.students(ID,firstName,lastName,email) VALUES (" + id + ", '" +firstName+"','" +lastName+"','" +email+"');";
+
 		try {
 			Statement stmt = connect.createStatement();
 			stmt.executeUpdate(sql);
@@ -44,46 +44,45 @@ public class Student {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//return affected;
+		
 	}
 	public void readAll() {
-       int affected =0;
+		int affected =0;
 		try {
 			String strSelect = "select * from dblab.students";
 			PreparedStatement ps = connect.prepareStatement(strSelect);
-				ResultSet  result = ps.executeQuery();
-				
-					while( result.next()) {
-						
-						System.out.println(result.getString(1)+ " "+result.getString(2));
-						
-					}
-			 
+			ResultSet  result = ps.executeQuery();
+
+			while( result.next()) {
+				System.out.println("ID FIRSTNAME LASTNAME EMAIL");
+				System.out.println( result.getString(1)+ "    "+result.getString(2)+ "    "+result.getString(3)+ "   "+result.getString(4));
+
+			}
+
 		} catch (SQLException e) { 
 			e.printStackTrace();
-		}
+		} 
 
 
 	} 
-	
-	 public void update(String id, String name) {
-		 
-	int affected =0;
- 
-		 try {
-			 String strUpdate = "UPDATE dblab.students set name = ? where id = ?";
+
+	public void update(String id,String firstName) {
+
+
+		try {
+			String strUpdate = "UPDATE dblab.students set firstName = ? where id = ?";
 			PreparedStatement ps = connect.prepareStatement(strUpdate);
-			ps.setString(1, name);
+			ps.setString(1, firstName);
 			ps.setString(2, id);
 			ps.executeUpdate();
-			 
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	 }
+	}
 	public void delete(String id) {
-		
+
 
 		try { 
 			String sqlDelete = "DELETE from dblab.students where id = "+ id;
